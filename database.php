e4c36d16cc170dbff72ce672a8ad951a0fa6c7aa
wisnuwicaksn <adiwisnu123@gmail.com>
2023-06-20 22:13:19 +0700

    update database

--- database.php
+++ database.php
@@ -14,30 +14,73 @@ class Database {
             die("Connection failed: " . $this->conn->connect_error);
         }
     }
-}
 
-private function getConfig(){
-    include_once("config.php");
-    $this->host = $config['host'];
-    $this->user = $config['username'];
-    $this->password = $config['password'];
-    $this->db_name = $config['db_name'];
-}
 
-public function query($sql){
-    return $this->conn->query($sql);
-}
-
-public function get($table, $where=null){
-    if ($where){
-        $where = " WHERE " .$where;
+    private function getConfig(){
+        include_once("config.php");
+        $this->host = $config['host'];
+        $this->user = $config['username'];
+        $this->password = $config['password'];
+        $this->db_name = $config['db_name'];
     }
-    $sql = " SELECT * FROM " .$table.$where;
-    $sql = $this->$conn->query($sql);
-    $sql = $sql->fetch_assoc();
-    return $sql;
-}
 
-public function insert($table, $data) {
-    
+    public function query($sql){
+        return $this->conn->query($sql);
+    }
+
+    public function get($table, $where=null){
+        if ($where){
+            $where = " WHERE " .$where;
+        }
+        $sql = " SELECT * FROM " .$table.$where;
+        $sql = $this->$conn->query($sql);
+        $sql = $sql->fetch_assoc();
+        return $sql;
+    }
+
+    public function insert($table, $data) {
+        if (is_array($data)){
+            foreach ($data as $key => $val) {
+                $colomn[] = $key;
+                $value[] = implode(",", $value);
+            }
+            $columns = implode(",", $column);
+            $values = implode(",", $value);
+        }
+        $sql = "INSERT INTO ".$table." (".$columns.") VALUES (".$values.")";
+        $sql = $this->conn->query($sql);
+        if ($sql == true) {
+            return $sql;
+        } else {
+            return false;
+        }
+    }
+
+    public function update($table, $data, $where){
+        $update_value = "";
+        if (is_array($data)) {
+            foreach($data as $key => $val) {
+            $update_value[] = "$key='{$val}'"
+            }   
+            $update_value = implode(",", $update_value);
+        }
+        $sql = "UPDATE ".$table." SET ".$update_value." WHERE ".$where;
+        $sql = $this->conn->query($sql);
+        if ($sql == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete($table, $filter) {
+        $sql = "DELETE FROM ".$table." ".$filter;
+        $sql = $this->conn->query($sql);
+        if ($sql == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+?>
